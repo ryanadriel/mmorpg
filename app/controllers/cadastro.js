c5c012d1e2ddc8b485beb0233e4a68b0af4f7c7e
Adriel Ryan <adrielryan.tj@hotmail.com>
2021-09-24 14:45:33 -0300

    receiving and validating data

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,4 +1,21 @@
 module.exports.cadastro = function (application, req, res){
+    res.render('cadastro', {validacao: {}, dadosForm:{}});
+}
 
-    res.render('cadastro');
+module.exports.cadastrar = function (application, req, res){
+    
+    var dadosForm = req.body;
+
+    req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazio').notEmpty();
+    req.assert('casa', 'Casa não pode ser vazio').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('cadastro', {validacao: erros, dadosForm: dadosForm});
+        return;
+    }
+    res.send('podemos cadastrar')
 }
