ed1f74f68a6343dc2e92eafb9a352c8772e10312
Adriel Ryan <adrielryan.tj@hotmail.com>
2021-10-05 18:30:39 -0300

    implementing session.destroy()

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,15 @@
 module.exports.jogo = function (application, req, res){
-    res.render('jogo');
+
+    if(req.session.autorizado){
+        res.render('jogo');
+    } else {
+        res.send('Usuário precisa fazer login');
+    }
+}
+
+module.exports.sair = function (application, req, res){
+
+    req.session.destroy( function(err){
+        res.render("index", {validacao: {}});
+    });
 }
