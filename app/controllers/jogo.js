e6316fd29f367d3f828e523fee3cc922d3d387d5
Adriel Ryan <adrielryan.tj@hotmail.com>
2021-10-12 08:23:57 -0300

    retrieving database actions

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -41,6 +41,13 @@ module.exports.pergaminhos = function (application, req, res){
         return;   
     }
 
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    var usuario = req.session.usuario;
+
+    JogoDAO.getAcoes(usuario);
+
     res.render("pergaminhos", {validacao: {}});
 }
 
