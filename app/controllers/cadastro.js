4f86d3024b326d3e98a5ee34fa5b8f7781f92e88
Adriel Ryan <adrielryan.tj@hotmail.com>
2021-10-12 08:10:03 -0300

    adding and saving in game actions database

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -20,8 +20,11 @@ module.exports.cadastrar = function (application, req, res) {
 
     var connection = application.config.dbConnection;  
     var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+    var JogoDAO = new application.app.models.JogoDAO(connection);
 
     UsuariosDAO.inserirUsuario(dadosForm);
+    JogoDAO.gerarParametros(dadosForm.usuario);
+
 
     res.send('podemos cadastrar')
 }
--- app/routes/jogo.js
+++ app/routes/jogo.js
@@ -7,4 +7,18 @@ module.exports = function(application){
 		application.app.controllers.jogo.sair(application, req, res);
 	});
 
+	application.get('/suditos', function(req, res){
+		application.app.controllers.jogo.suditos(application, req, res);
+	});
+
+	application.get('/pergaminhos', function(req, res){
+		application.app.controllers.jogo.pergaminhos(application, req, res);
+	});
+
+	application.post('/ordernar_acao_sudito', function(req, res){
+		application.app.controllers.jogo.ordernar_acao_sudito(application, req, res);
+	});
+
+
+	
 }
